acb4774a0be53c2a76919e316d5e795883449433
Erik-42 <mesen.erik@gmail.com>
2024-01-26 19:41:00 +0100

    Ajout page photographe

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,7 +1,7 @@
 function photographerTemplate(data) {
     const { name, portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `./assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
