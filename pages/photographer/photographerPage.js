a7c37b536a95662d823954f6ee8573f44d87a99a
Erik-42 <mesen.erik@gmail.com>
2024-02-12 08:25:14 +0100

    Ajout dom photographer

--- pages/photographer/photographerPage.js
+++ pages/photographer/photographerPage.js
@@ -1,33 +1,33 @@
-
-import {getPhotographers} from '../../scripts/utils/getData.js'
+import { getPhotographers } from "../../scripts/utils/getData.js";
+import { photographerPageTemplate } from "../../scripts/templates/photographerPageTemplate.js";
 
 async function displayData(photographers) {
-    const photographersHeader = document.querySelector(
-      ".photographer_container"
-    );
+  const photographersHeader = document.querySelector(".photographer_container");
 
-    photographers.forEach((photographer) => {
-        // const photographerModel = new photographerPageTemplate(photographer);
-        // const userCardDOM = photographerModel.getUserCardDOM();
-        // photographersHeader.appendChild(userCardDOM);
-        const photographerFactory = new photographerFactory(photographer);
-        const photographerBanner = photographerFactory.photographerBannerElement();
+  photographers.forEach((photographer) => {
+    // const photographerModel = new photographerPageTemplate(photographer);
+    // const userCardDOM = photographerModel.getUserCardDOM();
+    // photographersHeader.appendChild(userCardDOM);
+    const photographerPageModel = new photographerPageFactory(photographer);
+    const photographerBanner = photographerPageModel.photographerDomBanner();
 
-        photographersHeader.appendChild(photographerBanner);
-    });
+    photographersHeader.appendChild(photographerBanner);
+  });
 }
 
 async function init() {
-    const url = new URL(window.location);
-    const idPhotographer = url.searchParams.get('id')
-    // Récupère les datas des photographes
-    const data = await getPhotographers();
-    const photographer = data.photographers.filter(p => p.id == idPhotographer)[0]
-    const medias = data.media.filter(m =>m.photographerId==idPhotographer)
-    console.log(medias)
-    // displayData(photographers);
-    displayHeader(photographer)
-    displayMedias(medias)
+  const url = new URL(window.location);
+  const idPhotographer = url.searchParams.get("id");
+  // Récupère les datas des photographes
+  const data = await getPhotographers();
+  const photographer = data.photographers.filter(
+    (p) => p.id == idPhotographer
+  )[0];
+  const medias = data.media.filter((m) => m.photographerId == idPhotographer);
+  console.log(medias);
+  displayData(photographers);
+  displayHeader(photographer);
+  displayMedias(medias);
 }
 
 init();
--- scripts/templates/photographerPageTemplate.js
+++ scripts/templates/photographerPageTemplate.js
@@ -1,30 +1,30 @@
-class photographerFactory {
-    constructor(data) {
-        this.data = data
-        this.urlPhotographer = `photographerPage.html?=${data.id}`
-        this.photographerPortrait = `assets/photographers/${data.portrait}`
-        this.photographerName = `${data.name}`
-        this.location = `${data.city}, ${data.country}`
-        this.tagline = `${data.tagline}`
-        this.price = `${data.price}€/jour`
-    }
+export class photographerPageFactory {
+  constructor(data) {
+    this.data = data;
+    this.urlPhotographer = `../../pages/photographer/photographerPage.html?=${data.id}`;
+    this.photographerPortrait = `../../assets/photographers/${data.portrait}`;
+    this.photographerName = `../../assets/photographers/${data.name}`;
+    this.location = `../../assets/photographers/${data.city}, ../../assets/photographers/${data.country}`;
+    this.tagline = `../../assets/photographers/${data.tagline}`;
+    this.price = `../../assets/photographers/${data.price}€/jour`;
+  }
 
-    photographerBannerElements() {
-        const photographerBannerStructure = `
-        < section class = "photographer__card">
-            < h2 class = "photographer__card__name">${this.photographerName}</h2>
-            <p class="photographer__card__location">${this.location}</p>
-            <p class = "photographer__card__text" >${this.tagline}</p>
+  photographerDomBanner() {
+    const photographerBannerStructure = `
+        < section class = "photographer_header">
+            < h2 class = "photographer_header__name">${this.photographerName}</h2>
+            <p class="photographer_header__location">${this.location}</p>
+            <p class = "pphotographer_header__text" >${this.tagline}</p>
             < button class="contact_button contact-me-btn" aria-label="Contact me" tabindex="1" onclick="displayModal()" >Contactez-moi</button>
-            < div class = "photographer__portrait" >
-                < img src="${this.photographerPortrait}" alt="portrait photographe" class="photographer__portrait__img" >
+            < div class = "photographer_header__portrait" >
+                < img src="${this.photographerPortrait}" alt="portrait photographe" class="photographer_header__portrait__img" >
             </div>
         </section>
         `;
 
-        const container = document.createElement('div');
-        container.innerHTML = photographerBannerStructure;
+    const container = document.createElement("div");
+    container.innerHTML = photographerBannerStructure;
 
-        return container.firstChild;
-    }
+    return container.firstChild;
+  }
 }
