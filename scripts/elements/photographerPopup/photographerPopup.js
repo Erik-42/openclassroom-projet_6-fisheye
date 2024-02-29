a403aaf1dbb84d85329702f112816616c9830ba3
Erik-42 <mesen.erik@gmail.com>
2024-02-29 13:53:20 +0100

    modal html

--- scripts/elements/photographerPopup/photographerPopup.js
+++ scripts/elements/photographerPopup/photographerPopup.js
@@ -1,53 +1,53 @@
-import { getPhotographers } from "../../utils/getData.js";
-import { getMediaAndName } from "../../utils/getMediaAndName.js";
-import { photographerTemplate } from "../../templates/photographerTemplate.js";
+// import { getPhotographers } from "../../utils/getData.js";
+// import { getMediaAndName } from "../../utils/getMediaAndName.js";
+// import { photographerTemplate } from "../../templates/photographerTemplate.js";
 
-export function additionLike(photographerId, data) {
-  // Filtrer les médias du photographe spécifié
-  const mediasPhotographe = getMediaAndName(photographerId, data);
+// export function additionLike(photographerId, data) {
+//   // Filtrer les médias du photographe spécifié
+//   const mediasPhotographe = getMediaAndName(photographerId, data);
 
-  // Additionner les likes de chaque photo
-  const sommeLikes = mediasPhotographe.reduce(
-    (totalLikes, media) => totalLikes + media.likes,
+//   // Additionner les likes de chaque photo
+//   const sommeLikes = mediasPhotographe.reduce(
+//     (totalLikes, media) => totalLikes + media.likes,
     
-  );
+//   );
 
-  return sommeLikes;
-}
+//   return sommeLikes;
+// }
 
-function ajoutLike() {
-    let heart = document.querySelector(".photograph__info__like__heart");
-    heart = addEventListener("click", (e) => {
-      const totalLikesElement = document.getElementById("totalLikes");
-      const currentLikes = parseInt(totalLikesElement.textContent);
-      totalLikesElement.textContent = currentLikes + 1;
-    });
+// function ajoutLike() {
+//     let heart = document.querySelector(".photograph__info__like__heart");
+//     heart = addEventListener("click", (e) => {
+//       const totalLikesElement = document.getElementById("totalLikes");
+//       const currentLikes = parseInt(totalLikesElement.textContent);
+//       totalLikesElement.textContent = currentLikes + 1;
+//     });
   
-}
+// }
 
-const photographerId = photographerId;
-const likesTotalPhotographe = additionLike(photographerId, data);
+// const photographerId = photographerId;
+// const likesTotalPhotographe = additionLike(photographerId, data);
 
-console.log(photographerId);
+// console.log(photographerId);
 
-let photograph_info = document.getElementById("photograph__popup");
+// let photograph_info = document.getElementById("photograph__popup");
 
-function popupPhotographer() {
-  const popupStructure = `
-         <div class="photograph__info__like"">
-          <div id="totalLikes" class="photograph__info__like__nbr">${likesTotalPhotographe}</div>
-          <div class="photograph__info__like__heart">
-          <i class="fas fa-heart"></i>
-          </div>
-        </div>
-        <div class="photograph__info__price">${photographerTemplate.price}/jour</div>
-        `;
+// function popupPhotographer() {
+//   const popupStructure = `
+//          <div class="photograph__info__like"">
+//           <div id="totalLikes" class="photograph__info__like__nbr">${likesTotalPhotographe}</div>
+//           <div class="photograph__info__like__heart">
+//           <i class="fas fa-heart"></i>
+//           </div>
+//         </div>
+//         <div class="photograph__info__price">${photographerTemplate.price}/jour</div>
+//         `;
 
-  const container = document.createElement("div");
-  container.classList.add("photograph__info");
-  container.innerHTML = popupStructure;
-  return container;
-}
+//   const container = document.createElement("div");
+//   container.classList.add("photograph__info");
+//   container.innerHTML = popupStructure;
+//   return container;
+// }
 
 
-photograph_info.appendChild(popupPhotographer());
+// photograph_info.appendChild(popupPhotographer());
